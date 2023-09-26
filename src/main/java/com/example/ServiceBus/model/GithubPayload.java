775905074477fde9a5c9470fe9b603337d896277
Saathvik <s.saathvik@capgemini.com>
2023-09-26 15:48:12 +0530

    model class alter

--- src/main/java/com/example/ServiceBus/model/GithubPayload.java
+++ src/main/java/com/example/ServiceBus/model/GithubPayload.java
@@ -17,269 +17,346 @@ public class GithubPayload {
     private List<Commit> commits;
     private Commit head_commit;
 
-
-
     public String getRef() {
         return ref;
     }
-
-
 
     public void setRef(String ref) {
         this.ref = ref;
     }
 
-
-
     public String getBefore() {
         return before;
     }
-
-
 
     public void setBefore(String before) {
         this.before = before;
     }
 
-
-
     public String getAfter() {
         return after;
     }
-
-
 
     public void setAfter(String after) {
         this.after = after;
     }
 
-
-
     public Repository getRepository() {
         return repository;
     }
-
-
 
     public void setRepository(Repository repository) {
         this.repository = repository;
     }
 
-
-
     public Pusher getPusher() {
         return pusher;
     }
-
-
 
     public void setPusher(Pusher pusher) {
         this.pusher = pusher;
     }
 
-
-
     public Sender getSender() {
         return sender;
     }
-
-
 
     public void setSender(Sender sender) {
         this.sender = sender;
     }
 
-
-
     public boolean isCreated() {
         return created;
     }
-
-
 
     public void setCreated(boolean created) {
         this.created = created;
     }
 
-
-
     public boolean isDeleted() {
         return deleted;
     }
-
-
 
     public void setDeleted(boolean deleted) {
         this.deleted = deleted;
     }
 
-
-
     public boolean isForced() {
         return forced;
     }
-
-
 
     public void setForced(boolean forced) {
         this.forced = forced;
     }
 
-
-
     public String getBase_ref() {
         return base_ref;
     }
-
-
 
     public void setBase_ref(String base_ref) {
         this.base_ref = base_ref;
     }
 
-
-
     public String getCompare() {
         return compare;
     }
-
-
 
     public void setCompare(String compare) {
         this.compare = compare;
     }
 
-
-
     public List<Commit> getCommits() {
         return commits;
     }
-
-
 
     public void setCommits(List<Commit> commits) {
         this.commits = commits;
     }
 
-
-
     public Commit getHead_commit() {
         return head_commit;
     }
-
-
 
     public void setHead_commit(Commit head_commit) {
         this.head_commit = head_commit;
     }
 
-
-
     public static class Repository {
-        private String id;
+        private long id;
         private String name;
         private String full_name;
+        private boolean privateRepo;
+        private Owner owner;
 
-
-
-        public String getId() {
+        public long getId() {
             return id;
         }
 
-
-
-        public void setId(String id) {
+        public void setId(long id) {
             this.id = id;
         }
-
-
 
         public String getName() {
             return name;
         }
 
-
-
         public void setName(String name) {
             this.name = name;
         }
-
-
 
         public String getFull_name() {
             return full_name;
         }
 
-
-
         public void setFull_name(String full_name) {
             this.full_name = full_name;
         }
+
+        public boolean isPrivateRepo() {
+            return privateRepo;
+        }
+
+        public void setPrivateRepo(boolean privateRepo) {
+            this.privateRepo = privateRepo;
+        }
+
+        public Owner getOwner() {
+            return owner;
+        }
+
+        public void setOwner(Owner owner) {
+            this.owner = owner;
+        }
     }
 
-
-
-    public static class Pusher {
+    public static class Owner {
         private String name;
         private String email;
-
-
 
         public String getName() {
             return name;
         }
 
-
-
         public void setName(String name) {
             this.name = name;
         }
 
-
-
         public String getEmail() {
             return email;
         }
-
-
 
         public void setEmail(String email) {
             this.email = email;
         }
     }
 
+    public static class Pusher {
+        private String name;
+        private String email;
 
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
 
     public static class Sender {
         private String login;
-        private int id;
-
-
+        private long id;
+        private String node_id;
+        private String avatar_url;
+        private String gravatar_id;
+        private String url;
+        private String html_url;
+        private String followers_url;
+        private String following_url;
+        private String gists_url;
+        private String starred_url;
+        private String subscriptions_url;
+        private String organizations_url;
+        private String repos_url;
+        private String events_url;
+        private boolean site_admin;
 
         public String getLogin() {
             return login;
         }
 
-
-
         public void setLogin(String login) {
             this.login = login;
         }
 
-
-
-        public int getId() {
+        public long getId() {
             return id;
         }
 
-
-
-        public void setId(int id) {
+        public void setId(long id) {
             this.id = id;
         }
+
+        public String getNode_id() {
+            return node_id;
+        }
+
+        public void setNode_id(String node_id) {
+            this.node_id = node_id;
+        }
+
+        public String getAvatar_url() {
+            return avatar_url;
+        }
+
+        public void setAvatar_url(String avatar_url) {
+            this.avatar_url = avatar_url;
+        }
+
+        public String getGravatar_id() {
+            return gravatar_id;
+        }
+
+        public void setGravatar_id(String gravatar_id) {
+            this.gravatar_id = gravatar_id;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getHtml_url() {
+            return html_url;
+        }
+
+        public void setHtml_url(String html_url) {
+            this.html_url = html_url;
+        }
+
+        public String getFollowers_url() {
+            return followers_url;
+        }
+
+        public void setFollowers_url(String followers_url) {
+            this.followers_url = followers_url;
+        }
+
+        public String getFollowing_url() {
+            return following_url;
+        }
+
+        public void setFollowing_url(String following_url) {
+            this.following_url = following_url;
+        }
+
+        public String getGists_url() {
+            return gists_url;
+        }
+
+        public void setGists_url(String gists_url) {
+            this.gists_url = gists_url;
+        }
+
+        public String getStarred_url() {
+            return starred_url;
+        }
+
+        public void setStarred_url(String starred_url) {
+            this.starred_url = starred_url;
+        }
+
+        public String getSubscriptions_url() {
+            return subscriptions_url;
+        }
+
+        public void setSubscriptions_url(String subscriptions_url) {
+            this.subscriptions_url = subscriptions_url;
+        }
+
+        public String getOrganizations_url() {
+            return organizations_url;
+        }
+
+        public void setOrganizations_url(String organizations_url) {
+            this.organizations_url = organizations_url;
+        }
+
+        public String getRepos_url() {
+            return repos_url;
+        }
+
+        public void setRepos_url(String repos_url) {
+            this.repos_url = repos_url;
+        }
+
+        public String getEvents_url() {
+            return events_url;
+        }
+
+        public void setEvents_url(String events_url) {
+            this.events_url = events_url;
+        }
+
+        public boolean isSite_admin() {
+            return site_admin;
+        }
+
+        public void setSite_admin(boolean site_admin) {
+            this.site_admin = site_admin;
+        }
     }
-
-
 
     public static class Commit {
         private String id;
@@ -294,139 +371,93 @@ public class GithubPayload {
         private List<String> removed;
         private List<String> modified;
 
-
-
         public String getId() {
             return id;
         }
-
-
 
         public void setId(String id) {
             this.id = id;
         }
 
-
-
         public String getTree_id() {
             return tree_id;
         }
-
-
 
         public void setTree_id(String tree_id) {
             this.tree_id = tree_id;
         }
 
-
-
         public boolean isDistinct() {
             return distinct;
         }
-
-
 
         public void setDistinct(boolean distinct) {
             this.distinct = distinct;
         }
 
-
-
         public String getMessage() {
             return message;
         }
-
-
 
         public void setMessage(String message) {
             this.message = message;
         }
 
-
-
         public String getTimestamp() {
             return timestamp;
         }
-
-
 
         public void setTimestamp(String timestamp) {
             this.timestamp = timestamp;
         }
 
-
-
         public String getUrl() {
             return url;
         }
-
-
 
         public void setUrl(String url) {
             this.url = url;
         }
 
-
-
         public Author getAuthor() {
             return author;
         }
-
-
 
         public void setAuthor(Author author) {
             this.author = author;
         }
 
-
-
         public Author getCommitter() {
             return committer;
         }
-
-
 
         public void setCommitter(Author committer) {
             this.committer = committer;
         }
 
-
-
         public List<String> getAdded() {
             return added;
         }
-
-
 
         public void setAdded(List<String> added) {
             this.added = added;
         }
 
-
-
         public List<String> getRemoved() {
             return removed;
         }
-
-
 
         public void setRemoved(List<String> removed) {
             this.removed = removed;
         }
 
-
-
         public List<String> getModified() {
             return modified;
         }
 
-
-
         public void setModified(List<String> modified) {
             this.modified = modified;
         }
-
-
 
         public static class Author {
             private String name;
