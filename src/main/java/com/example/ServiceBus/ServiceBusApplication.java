90fbb0c342077b7936c36c54ac5ba335ae66e82b
Saathvik <s.saathvik@capgemini.com>
2023-09-29 17:33:06 +0530

    Initial changes

--- src/main/java/com/example/ServiceBus/ServiceBusApplication.java
+++ src/main/java/com/example/ServiceBus/ServiceBusApplication.java
@@ -1,9 +1,12 @@
 package com.example.ServiceBus;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info= @Info(title = "Service Applicaion", version = "3.0",description = "ServiceBus Application"))
 public class ServiceBusApplication {
 
 	public static void main(String[] args) {
