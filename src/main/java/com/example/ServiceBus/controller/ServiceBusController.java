3554f73941d1e17cf8190d1b5b6502d38be425c8
Saathvik <s.saathvik@capgemini.com>
2023-10-04 10:53:02 +0530

    Test Commit

--- src/main/java/com/example/ServiceBus/controller/ServiceBusController.java
+++ src/main/java/com/example/ServiceBus/controller/ServiceBusController.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api")
 public class ServiceBusController {
-    String sname="Saathvik";
+    String sname="SaathvikJava";
 
     private final GithubPayloadService payloadService;
     @Autowired
